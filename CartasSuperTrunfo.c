8d3798c9bb1c75e0f9353d0f379d5e9e028e912b
FelipePinheiroFerreira <202501212905@alunos.estacio.br>
2025-04-17 09:52:45 +0000

    Leitura dos dados da carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // Declaração das variáveis para a carta 1
+    // 
     char estado1;
     char codigo1[4];
     char nomeCidade1[100];
@@ -18,6 +18,30 @@ int main() {
      float area2;
      float pib2;
      int pontosTuristicos2;
-     
+
+     // Leitura dos dados da carta 1
+    printf("Cadastro da Carta 1:\n");
+
+    printf("Informe o Estado (letra de A a H): ");
+    scanf(" %c", &estado1);
+
+    printf("Informe o Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Informe o Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade1);  // lê a string com espaços
+
+    printf("Informe a População: ");
+    scanf("%d", &populacao1);
+
+    printf("Informe a Área (em km²): ");
+    scanf("%f", &area1);
+
+    printf("Informe o PIB (em bilhões de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Informe o Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
 return 0;
 } 
