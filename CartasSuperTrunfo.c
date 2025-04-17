99c1666b28ca59c04cd21e913d15650ec09aaa99
FelipePinheiroFerreira <202501212905@alunos.estacio.br>
2025-04-17 09:48:33 +0000

    Declaração das variáveis para a carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,15 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
- 
+    
+     // Declaração das variáveis para a carta 2
+     char estado2;
+     char codigo2[4];
+     char nomeCidade2[100];
+     int populacao2;
+     float area2;
+     float pib2;
+     int pontosTuristicos2;
+     
 return 0;
 } 
