32abc58258962bbd8d7281dc32edfea909e461d4
FelipePinheiroFerreira <202501212905@alunos.estacio.br>
2025-04-17 06:31:49 -0300

    Update CartasSuperTrunfo.c (Declaração das variáveis para a carta 1)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main() {
-    printf ("Desafio Super Trunfo - Países!\n");
-    
-    return 0;
-} 
 
+int main() {
+    // Declaração das variáveis para a carta 1
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[100];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+return 0;
+} 
