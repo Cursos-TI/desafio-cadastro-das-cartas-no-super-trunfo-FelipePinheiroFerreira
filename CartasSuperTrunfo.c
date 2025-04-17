cf1cce272df0d3b3a905fdb84cfcbc4f21e522f4
FelipePinheiroFerreira <202501212905@alunos.estacio.br>
2025-04-17 09:53:49 +0000

    Exibição dos dados da carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -66,5 +66,15 @@ int main() {
 
     printf("Informe o Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos2);
+
+     // Exibição dos dados da carta 1
+     printf("\nCarta 1:\n");
+     printf("Estado: %c\n", estado1);
+     printf("Código: %s\n", codigo1);
+     printf("Nome da Cidade: %s\n", nomeCidade1);
+     printf("População: %d\n", populacao1);
+     printf("Área: %.2f km²\n", area1);
+     printf("PIB: %.2f bilhões de reais\n", pib1);
+     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 return 0;
 } 
